4ad172d37957b93147f3dcc53b3498c48c7b20f0
FrostBlade001 <udov2008@gmail.com>
2021-02-20 14:59:05 +0500

    Refactor task 3

--- lesson_3/task_3.py
+++ lesson_3/task_3.py
@@ -6,11 +6,8 @@
 
 def sum_of_biggest_two(a: int, b: int, c: int) -> int:
     nums = [a, b, c]
-    m_1 = max(nums)
-    nums.remove(m_1)
-    m_2 = max(nums)
 
-    return m_1 + m_2
+    return sum(nums) - min(nums)
 
 
 if __name__ == '__main__':
